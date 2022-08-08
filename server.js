3f583ee2e46ed59a130ee898bcadc48d50a849ac
allibrdoine <allibrodine81@gmail.com>
2022-08-08 18:59:43 -0500

    verified server

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+//import mysql2 package
+const mysql = require('mysql2');
 //import express
 const express = require('express');
 //port designation and app expression
@@ -8,8 +10,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '2011Jeep',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
-
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 //default response for any other request (Not Found)
 app.use((req, res) => {
